96d245265a9ba47fe44ec29ce6a2b2bc99a2ece4
JunWoo Seo <csjws@naver.com>
2026-02-10 17:38:47 +0900

    [Lv1]자연수 뒤집어 배열로 만들기_12932

---
+++ src/Programmers/Lv1/자연수뒤집어배열로만들기_12932/Solution.java
@@ -0,0 +1,13 @@
+package Programmers.Lv1.자연수뒤집어배열로만들기_12932;
+
+public class Solution {
+    public int[] solution(long n) {
+        String s = Long.toString(n);
+        int[] answer = new int[s.length()];
+        for(int i =0; i<s.length();i++){
+            answer[i]=s.charAt(s.length() -1 -i) -'0';
+        }
+
+        return answer;
+    }
+}
