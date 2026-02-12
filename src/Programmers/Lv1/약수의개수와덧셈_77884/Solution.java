e569aae1c6c3b9f188d7659b23518ba79f87e3d2
JunWoo Seo <csjws@naver.com>
2026-02-12 16:29:09 +0900

    [Lv1]약수의 개수와 덧셈_77884

---
+++ src/Programmers/Lv1/약수의개수와덧셈_77884/Solution.java
@@ -0,0 +1,21 @@
+package Programmers.Lv1.약수의개수와덧셈_77884;
+
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        for(int i =left; i<=right; i++){
+            int sum=0;
+            for(int j = 1; j<=i; j++){
+                if(i%j==0)
+                    sum++;
+            }
+            if(sum%2==0){
+                answer+=i;
+            }
+            else{
+                answer-=i;
+            }
+        }
+        return answer;
+    }
+}
