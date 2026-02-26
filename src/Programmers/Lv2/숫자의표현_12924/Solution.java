e0917869babaeaca98e5524ac482bb86f520502c
JunWoo Seo <csjws@naver.com>
2026-02-26 20:02:36 +0900

    [Lv2]숫자의 표현_12924

---
+++ src/Programmers/Lv2/숫자의표현_12924/Solution.java
@@ -0,0 +1,19 @@
+package Programmers.Lv2.숫자의표현_12924;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = i; j <= n; j++) {
+                sum += j;
+                if (sum == n) {
+                    answer++;
+                    break;
+                }
+                if (sum > n) break;
+            }
+        }
+        return answer;
+    }
+}
