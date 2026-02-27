5f26d70ba7bd528b9e2022bb8f4a043695d0b008
JunWoo Seo <csjws@naver.com>
2026-02-27 16:31:38 +0900

    [Lv2]JadenCase문자열 만들기_12951

---
+++ src/Programmers/Lv2/JadenCase문자열만들기_12951/Solution.java
@@ -0,0 +1,25 @@
+package Programmers.Lv2.JadenCase문자열만들기_12951;
+
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split(" ");
+        for(int i=0; i<arr.length; i++) {
+            String now = arr[i];
+            if(arr[i].length() == 0) {
+                answer += " ";
+            }
+            else {
+                answer += now.substring(0, 1).toUpperCase();
+                answer += now.substring(1, now.length()).toLowerCase();
+                answer += " ";
+            }
+        }
+        if(s.substring(s.length()-1, s.length()).equals(" ")){
+            return answer;
+        }
+
+        return answer.substring(0, answer.length()-1);
+    }
+}
