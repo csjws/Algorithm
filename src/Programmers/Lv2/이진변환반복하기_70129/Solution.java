f3daf3c7f56ec181252169965135275244d20237
JunWoo Seo <csjws@naver.com>
2026-03-04 14:18:52 +0900

    [Lv2]이진 변환 반복하기_70129

---
+++ src/Programmers/Lv2/이진변환반복하기_70129/Solution.java
@@ -0,0 +1,21 @@
+package Programmers.Lv2.이진변환반복하기_70129;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        while(s.length()>1){
+            int one =0;
+            for(int i =0; i<s.length();i++){
+                if(s.charAt(i)=='0'){
+                    answer[1]++;
+                }
+                else{
+                    one++;
+                }
+            }
+            s = Integer.toBinaryString(one);
+            answer[0]++;
+        }
+        return answer;
+    }
+}
