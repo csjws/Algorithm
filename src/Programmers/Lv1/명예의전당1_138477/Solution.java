1bab2903e9f8e3a686c3c7811cafd74daa637b8a
JunWoo Seo <csjws@naver.com>
2026-03-09 16:41:06 +0900

    [Lv1]명예의 전당 (1)_138477

---
+++ src/Programmers/Lv1/명예의전당1_138477/Solution.java
@@ -0,0 +1,19 @@
+package Programmers.Lv1.명예의전당1_138477;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for(int i = 0; i < score.length; i++) {
+            priorityQueue.add(score[i]);
+
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
+            }
+            answer[i] = priorityQueue.peek();
+        }
+        return answer;
+    }
+}
