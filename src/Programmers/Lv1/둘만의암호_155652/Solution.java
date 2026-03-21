66dd130a2fe2e5dab9451371fad10496564a778a
JunWoo Seo <csjws@naver.com>
2026-03-21 22:00:38 +0900

    [Lv1]둘만의 암호_155652

---
+++ src/Programmers/Lv1/둘만의암호_155652/Solution.java
@@ -0,0 +1,21 @@
+package Programmers.Lv1.둘만의암호_155652;
+
+class Solution {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            for (int j = 0; j < index; j++) {
+                c += 1;
+                if (c > 'z') {
+                    c -= 26;
+                }
+                if (skip.contains(String.valueOf(c))) {
+                    j--;
+                }
+            }
+            answer += c;
+        }
+        return answer;
+    }
+}
