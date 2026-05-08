f8f990365e2f6257f52a468d79e3393d6cdbb172
JunWoo Seo <csjws@naver.com>
2026-05-08 16:26:21 +0900

    [Lv1]내적_70128

---
+++ src/Programmers/Lv1/내적_70128/Solution.java
@@ -0,0 +1,11 @@
+package Programmers.Lv1.내적_70128;
+
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i =0; i< a.length; i++){
+            answer += a[i]*b[i];
+        }
+        return answer;
+    }
+}
