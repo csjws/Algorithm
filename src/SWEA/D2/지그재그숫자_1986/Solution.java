94e1b98c5a93c691b975393236678ccd17d11fb9
JunWoo Seo <csjws@naver.com>
2026-05-11 16:22:11 +0900

    [D2]지그재그 숫자_1986

---
+++ src/SWEA/D2/지그재그숫자_1986/Solution.java
@@ -0,0 +1,24 @@
+package SWEA.D2.지그재그숫자_1986;
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int n = sc.nextInt();
+            int sum =0;
+            for(int i =1; i<=n; i++){
+                if(i % 2 == 0 ) sum -= i;
+                else sum += i;
+            }
+            System.out.println("#"+ test_case + " " + sum);
+        }
+    }
+}
