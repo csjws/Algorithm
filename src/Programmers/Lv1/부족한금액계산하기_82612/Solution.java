d4494383334f0cf3862e3cc6f87ae3eefc1f1999
JunWoo Seo <csjws@naver.com>
2026-07-05 20:57:57 +0900

    [Lv1]부족한 금액 계산하기_82612

---
+++ src/Programmers/Lv1/부족한금액계산하기_82612/Solution.java
@@ -0,0 +1,20 @@
+package Programmers.Lv1.부족한금액계산하기_82612;
+
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        long total=0;
+
+        for(int i=1; i<count+1; i++){
+            total += price*i;
+        }
+
+        if(money<total)
+            answer = total-money;
+
+        else
+            answer = 0;
+
+        return answer;
+    }
+}
