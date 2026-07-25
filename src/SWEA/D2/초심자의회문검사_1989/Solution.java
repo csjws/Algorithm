d5ac90c7f1850ccfbcaef1eeed37d543557b4afb
junwooseo <csjws@naver.com>
2026-07-25 22:53:13 +0900

    [D2]초심자의 회문 검사_1989

---
+++ src/SWEA/D2/초심자의회문검사_1989/Solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.FileInputStream;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            String str = sc.next();
+            String reverse = new StringBuilder(str).reverse().toString();
+
+            if (str.equals(reverse))
+                System.out.println("#" + test_case + " 1");
+            else
+                System.out.println("#" + test_case + " 0");
+        }
+    }
+}
